a29b7d654dde44cf07ffa6029b10113f5798d9f4
Maddukuri Madhuri Nagalakshmi <nagalakshmi.22bce7094@vitapstudent.ac.in>
2026-01-08 11:26:36 +0530

    Adding new files

--- ai_skill_6M/Month1/Day5/marks_analyzer.py
+++ ai_skill_6M/Month1/Day5/marks_analyzer.py
@@ -1,17 +1,18 @@
 import marks_reader as m
+import math
 class MarksAnalyzer:
     def find_average(nums):
         if(len(nums)==0):
             return 0
         else:
-            sum = 0
+            total = 0
             for n in nums.keys():
-                sum += nums[n]
-            return int(sum/len(nums))
+                total += nums[n]
+            return int(total/len(nums))
     def find_highest(nums):
         if(len(nums)==0):
             return None
-        maxi = 0
+        maxi = -10000
         for n in nums.values():
             if(n>maxi):
                 maxi = n
@@ -19,10 +20,23 @@ class MarksAnalyzer:
     def find_lowest(nums):
         if(len(nums)==0):
             return None
-        mini = 100
+        mini = 100000
         for n in nums.values():
             if(n<mini):
                 mini = n
         return mini
     def count(nums):
         return len(nums)
+    def find_median(nums):
+        length = len(nums)
+        if(len(nums)==0):
+            return None
+        numbers = []
+        for k in nums.keys():
+            numbers.append(nums[k])
+        numbers.sort()
+        if(length%2!=0):
+            return numbers[length/2]
+        else:
+            mid = numbers[int(length/2)]+numbers[int(length/2+1)]
+            return mid/2
